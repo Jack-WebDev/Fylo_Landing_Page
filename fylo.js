1692ba2a7e548003ac8647d004af517a0dcf1f6e
Jack-WebDev <Jackbuda31@gmail.com>
2022-03-18 22:55:39 +0200

    Initial commit

---
+++ fylo.js
@@ -0,0 +1,12 @@
+
+const navMenu = document.querySelector('.nav-menu');
+const hamburger = document.querySelector('.hamburger');
+
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+})
+
+
+
